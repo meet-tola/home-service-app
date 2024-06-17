a45a1a8c10cb4a280baa13ec88e3e6163eee96f0
meet-tola <wasiusikiru7@gmail.com>
2024-06-17 12:19:10 +0100

    Shuffle ServiceList

--- app/_service/ServiceList.jsx
+++ app/_service/ServiceList.jsx
@@ -1,7 +1,14 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ServiceCard from "./ServiceCard";
 
 const ServiceList = ({ services }) => {
+  const [shuffledServices, setShuffledServices] = useState([]);
+
+  useEffect(() => {
+    if (services) {
+      setShuffledServices(shuffleArray(services));
+    }
+  }, [services]);
   return (
     <div>
       {services && services.length === 0 ? (
@@ -9,7 +16,7 @@ const ServiceList = ({ services }) => {
       ) : (
         <div className="mt-5">
           <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 mt-5">
-            {services.map((service) => (
+          {shuffledServices.map((service) => (
               <ServiceCard key={service._id} service={service} />
             ))}
           </div>
@@ -17,6 +24,15 @@ const ServiceList = ({ services }) => {
       )}
     </div>
   );
+};
+
+const shuffleArray = (array) => {
+  let shuffledArray = [...array];
+  for (let i = shuffledArray.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [shuffledArray[i], shuffledArray[j]] = [shuffledArray[j], shuffledArray[i]];
+  }
+  return shuffledArray;
 };
 
 export default ServiceList;
